399c7b75e629b5456e89685478ea4234e2e2a609
YifanZhou <yifan@Yifans-MacBook-Pro.local>
2015-12-01 16:34:41 -0500

    merged login server with ChatApp

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app      = express();
+//Chat
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var port     = process.env.PORT || 8080;
 
 var passport = require('passport');
@@ -42,8 +45,95 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 
 // routes ======================================================================
+
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+//========Chat App Starts here=========
+
+//Routing
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules/angular'));
+app.use(express.static(__dirname + '/node_modules/socket.io/node_modules/socket.io-client'));
+console.log(__dirname);
+
+// Chatroom : rooms which are currently available in chat
+var rooms = ['Lobby','room1','room2'];
+
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  console.log(__dirname);
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.in(socket.room).emit('new message', {username: socket.username,message: data});
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username, room) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+	socket.room = room; ////////////////////////////////////////////////////
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {numUsers: numUsers});
+    // echo globally (all clients) that a person has connected --> changed to room echo !
+    socket.broadcast.to(socket.room).emit('user joined', {username: socket.username,numUsers: numUsers, room: socket.room});
+	
+	////NEW STUFF HERE
+	socket.join('Lobby');
+	socket.broadcast.emit('updateroom', rooms, 'Lobby');
+  });
+  // NEW STUFF
+  socket.on('switchRoom', function(newroom){
+	  socket.broadcast.to(socket.room).emit('user left', {username: socket.username,numUsers: numUsers});
+	  socket.leave(socket.room);
+	  socket.join(newroom);
+	  socket.room = newroom;
+	  socket.broadcast.to(socket.room).emit('user joined', {username: socket.username, numUsers: numUsers, room: socket.room});
+	  //socket.emit('updateroom', rooms, newroom);	  
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {username: socket.username});
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {username: socket.username});
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {username: socket.username,numUsers: numUsers});
+	  socket.leave(socket.room); // NEW
+	}
+  });
+});
+
 // launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+// app.listen(port);
+// console.log('The magic happens on port ' + port);
+
+
+
+
+
+
